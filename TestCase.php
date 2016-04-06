21795070f86d5b0e812d87e4c37991f91a5651ac
Ngo Duy Anh <duyanh2812@gmail.com>
2016-04-06 23:46:29 +0700

    all commit

--- TestCase.php
+++ TestCase.php
@@ -9,7 +9,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     protected $baseUrl = 'http://localhost';
 
-    /**Creates the application.aaaa
+    /**Creates the application.
      *
      * @return \Illuminate\Foundation\Application
      */
