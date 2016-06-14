b3a43cc323a54f72a9a6a0664da37f56f4fad3a5
demonxxx <s9.jungsooyeon@gmail.com>
2016-06-15 00:10:29 +0700

    api feedback

--- app/Feedback.php
+++ app/Feedback.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Account extends Model
+class Feedback extends Model
 {
     use SoftDeletes;
 
@@ -14,23 +14,8 @@ class Account extends Model
      * @var array
      */
     protected $dates = ['deleted_at'];
-    protected $table = 'accounts';
+    protected $table = 'feedbacks';
     public $timestamps = true;
-
-    public function get_transaction_users($user_ids){
-        $users = [];
-        foreach ($user_ids as $user_id){
-            $user = DB::table("users")
-                    ->join("accounts", "accounts.user_id", "=", "users.id")
-                    ->where("users.deleted_at")
-                    ->where("accounts.deleted_at")
-                    ->where("users.id", $user_id)
-                    ->select("users.id", "users.code", "users.name", "accounts.main", "accounts.second")
-                    ->first();
-            array_push($users, $user);
-        }
-        return $users;
-    }
 
     
 }
