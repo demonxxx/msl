c20e92806d4e43c30d3ceb901b919bdbb945f711
Ngo Duy Anh <duyanh2812@gmail.com>
2016-06-24 22:19:23 +0700

    push message status

--- app/Http/Controllers/NotificationService.php
+++ app/Http/Controllers/NotificationService.php
@@ -45,6 +45,45 @@ trait NotificationService {
         fclose($fp);
     }
 
+    public function pushStatusOrder($deviceToken, $message) {
+        // just for test apple remote notification
+        /////////////////////////////////////////////////////////////////
+        // Put your device token here (without spaces):
+//        $deviceToken = '104f93b8f6323e24eff605bb15e860ffc919642b1f1fdf99aa6b36ddb6765f7b';
+
+        // Put your private key's passphrase here:
+        $passphrase = 'Abc@@123';
+
+        $ctx = stream_context_create();
+        stream_context_set_option($ctx, 'ssl', 'local_cert', base_path('server/') . IOS_CERTIFICATE_FILE);
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+        $fp = stream_socket_client(
+            'ssl://gateway.sandbox.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+        if ($fp) {
+            // Create the payload body
+            $url = 'http://13.76.129.137';
+            $body['aps'] = array(
+                'alert' => $message,
+                'sound' => 'default',
+                'link_url' => $url,
+                'category' => "NEWS_CATEGORY",
+            );
+
+            // Encode the payload as JSON
+            $payload = json_encode($body);
+
+            // Build the binary notification
+            $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+
+            // Send it to the server
+            $result = fwrite($fp, $msg, strlen($msg));
+
+            // Close the connection to the server
+        }
+        fclose($fp);
+    }
+
     /**
      * Sending Push Notification
      * $registatoin_ids an array id of user who get notification
